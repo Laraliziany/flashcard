62c10906e4ec3813ebf6eecc71f70c7ae1bff6d7
Laraliziany <rickli.lara@escola.pr.gov.br>
2025-08-21 09:14:33 -0300

    j,mj,

--- pergunta.js
+++ pergunta.js
@@ -1,22 +1,24 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+'Animais',
+'O que é mamifero´?',
+'Animais mamíferos são um grupo diversificado de vertebrados caracterizados pela presença de pelos e glândulas mamárias, que produzem leite para alimentar seus filhotes. Eles também são conhecidos por terem um cérebro desenvolvido, respiração pulmonar e sangue quente. 
+'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+'Geografia',
+'Qual a capital da França?',
+'A capital da França é Paris'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+'Programação',
+'O que é uma função?',
+'Uma função é um bloco de código que executa alguma tarefa'
 )
-    criaCartao(
-    'Lingua inglesa',
-    'Como se diz OI em inglês?',
-    'Oi em ingles é HI (RAI)'
+
+criaCartao(
+'Programação',
+'O que é uma função?',
+'Uma função é um bloco de código que executa alguma tarefa'
 )
